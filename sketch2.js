6d41bdc1f104da7e389bb32082debcce3be816e7
TBrown-2 <Taylor.Brown2@mail.citytech.cuny.edu>
2025-04-04 10:25:51 -0400

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -5,33 +5,30 @@ let pulseSpeed = 0.05;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  angleMode(RADIANS);
-  colorMode(HSB, 360, 100, 100);
+  angleMode(RADIANS); // optional, but good practice
 }
 
 function draw() {
   background(250);
-
   translate(width / 2, height / 2);
-
+  
   let d = 150 * sin(pulse);
 
   for (let i = 0; i < 10; i++) {
-    push();
-    rotate(r + TWO_PI * i / 10); // Evenly spaced rotation
+    rotate(r);
     translate(d, 80);
-    fill(200, 100, 100); // HSB: blue-ish
+    fill(0, 0, 100);
     stroke(100, 200, 50);
     strokeWeight(4);
     ellipse(0, 0, 50, 80);
-    pop();
+    translate(-d, -80); // move back before next iteration
   }
 
   pulse += pulseSpeed;
   r += rSpeed;
 }
 
-function mousePressed() {}
+function mousePressed() {
   if (rSpeed === 0) {
     rSpeed = 0.01;
     pulseSpeed = 0.05;
@@ -39,3 +36,8 @@ function mousePressed() {}
     rSpeed = 0;
     pulseSpeed = 0;
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
